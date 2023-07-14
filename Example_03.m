acf2bb322930f3a071a28ddc9178b2cc036fe9ab
hansjcv <63672703+hansjcv@users.noreply.github.com>
2023-07-14 15:49:26 +0200

    Example 3

---
+++ Example_03.m
@@ -0,0 +1,8 @@
+clear,addpath ../ ../Solutions/
+Temp  = linspace(300,1000,30) + 273.15;
+Pres  = linspace(0.1,4,31)*1e9;
+phase = {'Olivine','Orthopyroxene','q,tc-ds633','per,tc-ds633'};
+Cname = {'Si','Mg','Fe','O'};
+td    = init_thermo(phase,Cname,'solution_models_H18');
+[T,P] = ndgrid(Temp,Pres);
+[g,Npc,pc_id] = tl_gibbs_energy(T(:),P(:),phase,td);
