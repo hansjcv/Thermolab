8d22c87bb196b0a67dd2b618564c4e777f91aa5b
hansjcv <63672703+hansjcv@users.noreply.github.com>
2023-10-12 13:32:47 +0200

    Update Ex_rho_PS94.m

--- Examples/Ex_rho_PS94.m
+++ Examples/Ex_rho_PS94.m
@@ -1,6 +1,6 @@
 clear,clf,addpath ../ ../EOS/
-T = linspace(25,600,180) + 273.15;
-P = linspace(1,0.01e8,181);
+T = linspace(25,1000,180) + 273.15;
+P = linspace(1,2e9,181);
 [T2d,P2d] = ndgrid(T,P);
 td        = init_thermo({'H2O,tc-ds633'});
 [g0,v0]   = tl_g0(T2d(:),P2d(:),td);
@@ -8,3 +8,4 @@ molmW     = 18.0153e-3;
 rho_kgm3  = molmW./v0{1}*1e5;
 figure(1),colormap jet
 contourf(T2d-273.15,P2d/1e9,reshape(rho_kgm3,length(T),length(P)),30),colorbar,shading flat
+title('\rho_{H_2O} (kg/m^3)'),xlabel('T(\circC)'),ylabel('P(GPa)')
