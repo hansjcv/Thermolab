95fa95b585df86c8e9ad2f4afdea24f0f63b593d
hansjcv <63672703+hansjcv@users.noreply.github.com>
2024-05-31 14:49:44 +0200

    update v24_05_31

--- Figures/Fig2a_21_12_14.m
+++ Figures/Fig2a_21_12_14.m
@@ -1,9 +1,9 @@
-clear,hold on,addpath ../
+clear,clf,addpath ../
 T        = linspace(25,800,100)+273.15;
 P        = linspace(0.1,4,100)*1e9;
 Tr = 25 + 273.15;
 phases       = {'atg,tc-ds55','br,tc-ds55','fo,tc-ds55','H2O,tc-ds55','diam,tc-ds55','gph,tc-ds55','coe,tc-ds55','q,tc-ds55','an,tc-ds55','gr,tc-ds55','ky,tc-ds55'};
-i_react(1,:) = [1      1    1    1     0      0     0    0    0    0    0];symcol{1}='*b';leg_name{1} = 'atg + br = fo + H2O';
+i_react(1,:) = [1      1    1    1     0      0     0    0    0    0    0];symcol{1}='b';leg_name{1} = 'atg + br = fo + H2O';
 i_react(2,:) = [0      0    0    0     1      1     0    0    0    0    0];symcol{2}='g';leg_name{2} = 'diam = grph';
 i_react(3,:) = [0      0    0    0     0      0     1    1    0    0    0];symcol{3}='r';leg_name{3} = 'coe = q';
 i_react(4,:) = [0      0    0    0     0      0     0    1    1    1    1];symcol{4}='c';leg_name{4} = 'an = ky + gr + q';
