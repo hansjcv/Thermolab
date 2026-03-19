c81ac8b7040b4c8360a2411792830935915bb05a
hansjcv <j.c.vrijmoed@fu-berlin.de>
2026-03-19 22:43:37 +0100

    Figures update

--- Figures/Fig12_postprocess_v23_04_03_soapstone.m
+++ Figures/Fig12_postprocess_v23_04_03_soapstone.m
@@ -1,6 +1,5 @@
-clclear,figure(1),clf,colormap jet,addpath ../,addpath ../Utilities/
-clear,colormap jet
-runname  = 'soapstone_2022_02_14_n500_nz15_full_0_5kb'; 
+clear,figure(1),clf,colormap jet,addpath ../,addpath ../Utilities/
+runname  = 'soapstone_2022_02_14_n150_nz15_full_0_5kb'; 
 load(['linprog_run_' runname]);                                          % load linprog run data
 molm      = molmass_fun(Cname);
 solv_tol  = 100;
